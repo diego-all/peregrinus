40b69d45c652879b543de4c1a7c47ae2bfd25a8a
root <dposadallano@gmail.com>
2024-08-01 18:24:24 -0500

    folder structure

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Directorio de archivos estáticos
+	directorio := "./public"
+
+	// static files
+	fileServer := http.FileServer(http.Dir(directorio))
+
+	// Registrar el manejador de archivos en el servidor HTTP
+	http.Handle("/public/", http.StripPrefix("/public", fileServer))
+
+	// Iniciar el servidor en el puerto 8080
+	log.Println("Servidor de archivos escuchando en http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
