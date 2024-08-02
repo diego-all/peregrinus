fbf501260f192767d7b7da87e5b042b60ee57f79
root <dposadallano@gmail.com>
2024-08-02 15:50:21 -0500

    sitio in web server 8080

--- main.go
+++ main.go
@@ -7,13 +7,13 @@ import (
 
 func main() {
 	// Directorio de archivos estáticos
-	directorio := "./public"
+	directorio := "./sitio"
 
 	// static files
 	fileServer := http.FileServer(http.Dir(directorio))
 
 	// Registrar el manejador de archivos en el servidor HTTP
-	http.Handle("/public/", http.StripPrefix("/public", fileServer))
+	http.Handle("/sitio/", http.StripPrefix("/sitio", fileServer))
 
 	// Iniciar el servidor en el puerto 8080
 	log.Println("Servidor de archivos escuchando en http://localhost:8080")
