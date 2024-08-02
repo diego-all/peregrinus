bb0a29d49272cf11959ece5023446a53bd808b5b
root <dposadallano@gmail.com>
2024-08-01 21:07:54 -0500

    command execution 2 endpoints

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 
 
-    curl -X POST http://localhost:8080/execute -d 'cat ../../../../etc/passwd' -H 'Content-Type: text/plain'
+    curl -X POST http://localhost:8080/execute -d 'cat ../../../../etc/passwd' -H 'Content-Type: text/plain' (v1)
+
+
+
+    curl -X POST http://localhost:8080/execute-system -d 'cat /etc/passwd' -H 'Content-Type: text/plain'  (v2)
+
+    curl -X POST http://localhost:8080/execute-files -d 'cat etc/passwd' -H 'Content-Type: text/plain'  (v2)
--- main.go
+++ main.go
@@ -18,8 +18,11 @@ func main() {
 	// Registrar el manejador de archivos en el servidor HTTP
 	http.Handle("/public/", http.StripPrefix("/public", fileServer))
 
-	// Endpoint para ejecutar comandos
-	http.HandleFunc("/execute", executeCommand)
+	// Endpoint para ejecutar comandos en el contexto del sistema operativo
+	http.HandleFunc("/execute-system", executeCommandSystem)
+
+	// Endpoint para ejecutar comandos en el contexto del servidor de archivos
+	http.HandleFunc("/execute-files", executeCommandFiles)
 
 	// Iniciar el servidor en el puerto 8080
 	log.Println("Servidor de archivos escuchando en http://localhost:8080")
@@ -29,7 +32,8 @@ func main() {
 	}
 }
 
-func executeCommand(w http.ResponseWriter, r *http.Request) {
+// Endpoint para ejecutar comandos en el contexto del sistema operativo
+func executeCommandSystem(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
 		return
@@ -43,6 +47,30 @@ func executeCommand(w http.ResponseWriter, r *http.Request) {
 
 	cmdStr := string(body)
 	cmd := exec.Command("bash", "-c", cmdStr)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error al ejecutar el comando: %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(output)
+}
+
+// Endpoint para ejecutar comandos en el contexto del servidor de archivos
+func executeCommandFiles(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "No se pudo leer el cuerpo de la solicitud", http.StatusInternalServerError)
+		return
+	}
+
+	cmdStr := string(body)
+	cmd := exec.Command("bash", "-c", fmt.Sprintf("cd public && %s", cmdStr))
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Error al ejecutar el comando: %s", err), http.StatusInternalServerError)
