3ce7b36c84a83c468e626516dfae627d9bfcdc31
root <dposadallano@gmail.com>
2024-08-02 20:33:04 -0500

    reportHandler

--- main.go
+++ main.go
@@ -1,9 +1,48 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+type Report struct {
+	Command string `json:"command"`
+	Output  string `json:"output"`
+}
+
+func reportHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		http.Error(w, "Error al leer el cuerpo de la solicitud", http.StatusBadRequest)
+		return
+	}
+
+	defer r.Body.Close()
+
+	var report Report
+
+	if err := json.Unmarshal(body, &report); err != nil {
+		http.Error(w, "Error al parsear JSON", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Printf("Recibido reporte: %v\n", report)
+
+	w.WriteHeader(http.StatusOK)
+
+	w.Write([]byte("Reporte recibido correctamente"))
+
+}
 
 func main() {
 	// Directorio de archivos estáticos
@@ -14,6 +53,7 @@ func main() {
 
 	// Registrar el manejador de archivos en el servidor HTTP
 	http.Handle("/sitio/", http.StripPrefix("/sitio", fileServer))
+	http.HandleFunc("/report", reportHandler)
 
 	// Iniciar el servidor en el puerto 8080
 	log.Println("Servidor de archivos escuchando en http://localhost:8080")
